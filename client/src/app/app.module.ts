9505ce7661195bf2997609f6b822d9a38a2e103c
Cardan <11862549+Cardanont@users.noreply.github.com>
2021-12-02 14:55:48 -0600

    Basic display of companies on the application, still needs a lot of work

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,19 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CompanyCardComponent } from './company-card/company-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
